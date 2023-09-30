5b9cfeabe4a4bdf424dfd805155b6a76407cdc00
Natalia Schlebinger <natalia.schlebinger@gmail.com>
2023-09-30 20:48:51 -0300

    Handle secret with Docker

--- gaantrecipes/settings.py
+++ gaantrecipes/settings.py
@@ -11,16 +11,13 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k+=lpom(e7q+2#tbm@&b*5&c9_#++)u@i^=5g3iccw3-58)59u'
+with open(os.getenv('DJANGO_SECRET_FILE')) as f:
+    SECRET_KEY = f.read()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
